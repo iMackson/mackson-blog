73339a404d7c3968144da8d303a50e962dc19888
Mackson Isani <isanimackson@gmail.com>
2023-02-13 17:04:33 +0100

    Update on featured posts

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -3,19 +3,24 @@ import classes from "./post-item.module.css";
 
 const PostItem = (props) => {
   const { title, image, excerpt, date, slug } = props.post;
+
+  const formattedDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const imagePath = `/images/posts/{slug}/{image}`;
   return (
     <li className={classes.post}>
       <Link>
         <div className={classes.image}>
-          <Image />
+          <Image src={imagePath} alt={title} width={300} height={200} />
         </div>
         <div className={classes.content}>
-          <h3>TITLE</h3>
-          <time>February 13th 2023</time>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut,
-            facilis!
-          </p>
+          <h3>{title}</h3>
+          <time>{formattedDate}</time>
+          <p>{excerpt}</p>
         </div>
       </Link>
     </li>
